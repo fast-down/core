440413c651730acaa88f58efefa08ce86d11cf8d
share121 <2854631158@qq.com>
2025-02-16 16:46:02 +0800

    feat: 合并 fetch

--- worker.ts
+++ worker.ts
@@ -11,17 +11,33 @@ export interface FetchChunksOptions {
 export async function* fetchChunks(
   data: FetchChunksOptions
 ): AsyncGenerator<Uint8Array, void, unknown> {
-  for (const chunk of data.chunks) {
-    const r = await fetch(data.url, {
-      headers: {
-        ...data.headers,
-        Range: `bytes=${chunk.start}-${chunk.end}`,
-      },
-      signal: data.signal,
-      priority: "high",
-      proxy: data.proxy,
-    });
-    if (r.status !== 206) throw new Error(`不支持 Range 头`);
-    yield r.bytes();
+  const r = await fetch(data.url, {
+    headers: {
+      ...data.headers,
+      Range: `bytes=${data.chunks[0].start}-${data.chunks.at(-1)!.end}`,
+    },
+    signal: data.signal,
+    priority: "high",
+    proxy: data.proxy,
+  });
+  if (r.status !== 206) throw new Error(`不支持 Range 头`);
+  if (!r.body) throw new Error(`body 为空`);
+  let buffer: Uint8Array = new Uint8Array();
+  let i = 0;
+  for await (const respondChunk of r.body) {
+    const temp = new Uint8Array(buffer.length + respondChunk.length);
+    temp.set(buffer);
+    temp.set(respondChunk, buffer.length);
+    buffer = temp;
+    let offset = 0;
+    while (true) {
+      const chunk = data.chunks[i];
+      const chunkSize = chunk.end - chunk.start + 1;
+      if (buffer.byteLength < chunkSize) break;
+      yield buffer.slice(offset, offset + chunkSize);
+      offset += chunkSize;
+      i++;
+    }
+    buffer = buffer.slice(offset);
   }
 }
