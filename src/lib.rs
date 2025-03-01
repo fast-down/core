710dcb1374352c32743a4d6143d2ca85153f43cf
share121 <2854631158@qq.com>
2025-03-01 08:26:42 +0800

    feat: 实现 get_url_info 函数

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::{error::Error, sync::mpsc, thread};
 
+use content_disposition::parse_content_disposition;
 use reqwest::{
     blocking::Client,
-    header::{self, HeaderMap},
-    Proxy,
+    header::{self, HeaderMap, HeaderValue},
+    Proxy, StatusCode,
 };
 
 pub struct DownloadOptions<'a> {
@@ -43,17 +44,50 @@ pub fn download(options: DownloadOptions) -> Result<String, Box<dyn Error>> {
 
 pub struct UrlInfo {
     pub file_size: usize,
-    pub content_type: String,
     pub file_name: Option<String>,
+    pub can_use_range: bool,
+    pub final_url: String,
+    pub etag: Option<String>,
+    pub last_modified: Option<String>,
 }
 
 pub fn get_url_info(client: Client, url: &str) -> Result<UrlInfo, Box<dyn Error>> {
-    let headers = HeaderMap::new();
+    let mut headers = HeaderMap::new();
     headers.insert(header::RANGE, header::HeaderValue::from_static("bytes=0-"));
     let resp = client.get(url).headers(headers).send()?;
-    if resp.status().is_success() {
-        let headers = resp.headers()?;
+    let status = resp.status();
+    if status.is_success() {
+        let can_use_range = status == StatusCode::PARTIAL_CONTENT;
+        let resp_headers = resp.headers();
+        let file_size: usize = resp_headers
+            .get(reqwest::header::CONTENT_LENGTH)
+            .map_or(Ok("0"), |e| e.to_str())?
+            .parse()?;
+        let etag = resp_headers
+            .get(reqwest::header::ETAG)
+            .and_then(|e| e.to_str().ok())
+            .map(|e| e.to_string());
+        let file_name = resp_headers
+            .get(reqwest::header::CONTENT_DISPOSITION)
+            .and_then(|e| e.to_str().ok())
+            .and_then(|e| parse_content_disposition(e).filename_full());
+        let last_modified = resp_headers
+            .get(reqwest::header::LAST_MODIFIED)
+            .and_then(|e| e.to_str().ok())
+            .map(|e| e.to_string());
+        return Ok(UrlInfo {
+            file_name,
+            file_size,
+            can_use_range,
+            etag,
+            last_modified,
+            final_url: resp.url().to_string(),
+        });
     } else {
-        return Err("Failed to get URL info".into());
+        return Err(format!(
+            "Error: Failed to get URL info. status code: {}",
+            status.as_str()
+        )
+        .into());
     }
 }
