ae9918ae778fcb454458c36d36bf6a7560f525fd
share121 <2854631158@qq.com>
2025-05-16 10:32:32 +0800

    test: 添加测试

--- fast-down-cli/src/fmt_time.rs
+++ fast-down-cli/src/fmt_time.rs
@@ -4,3 +4,19 @@ pub fn format_time(time: u64) -> String {
     let hours = time / 3600;
     format!("{:02}:{:02}:{:02}", hours, minutes, seconds)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_format_time() {
+        assert_eq!(format_time(0), "00:00:00");
+        assert_eq!(format_time(59), "00:00:59");
+        assert_eq!(format_time(60), "00:01:00");
+        assert_eq!(format_time(3599), "00:59:59");
+        assert_eq!(format_time(3600), "01:00:00");
+        assert_eq!(format_time(3661), "01:01:01");
+        assert_eq!(format_time(86399), "23:59:59");
+    }
+}
