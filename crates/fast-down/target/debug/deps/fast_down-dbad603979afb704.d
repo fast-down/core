9c0728477e33c4e9197025bd799b394eab2b87c5
share121 <2854631158@qq.com>
2025-08-10 15:24:31 +0800

    chore: 移除 target 文件夹

--- crates/fast-down/target/debug/deps/fast_down-dbad603979afb704.d
+++
@@ -1,5 +0,0 @@
-/home/cyan/RustroverProjects/fast-down/fast-down/crates/fast-down/target/debug/deps/fast_down-dbad603979afb704.d: src/lib.rs
-
-/home/cyan/RustroverProjects/fast-down/fast-down/crates/fast-down/target/debug/deps/libfast_down-dbad603979afb704.rmeta: src/lib.rs
-
-src/lib.rs:
