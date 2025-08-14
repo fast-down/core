9944cdff95a59eaa0b5dd79cf91595d87f2a6854
Cyan <contact@cyans.me>
2025-08-14 21:38:56 +0800

    feat(fast-down/curl): fix flow control

--- crates/fast-down/curl.rs
+++ crates/fast-down/curl.rs
@@ -1,5 +1,8 @@
+use fast_down::curl::worker::{DataSignal, Op, multi, options, SIG_EVENT, STATE_SEND_FAILED};
+use kanal::ReceiveError;
+use std::sync::Arc;
 use std::thread;
-use fast_down::curl::worker::{multi, Op};
+use std::time::Duration;
 
 pub fn main() {
     let (tx_ops, rx_ops) = kanal::unbounded();
@@ -8,9 +11,39 @@ pub fn main() {
     });
     let (tx_ret, ret) = oneshot::channel();
     let (tx_data, rx_data) = kanal::bounded(1);
-    tx_ops.send(Op::New(tx_data, curl::easy::List::new(), "https://1.1.1.1/cdn-cgi/trace".to_string(), tx_ret)).unwrap();
-    for data in rx_data.into_iter() {
-        dbg!(String::from_utf8_lossy(&*data));
+    let signal: Arc<DataSignal> = Default::default();
+    tx_ops
+        .send(Op::New(
+            tx_data,
+            options::New {
+                signal: signal.clone(),
+                headers: curl::easy::List::new(),
+                url: "https://github.com/".to_string(),
+                extra: None,
+            },
+            tx_ret,
+        ))
+        .unwrap();
+    let th = ret.recv().unwrap();
+    loop {
+        if signal.is_send_failed() {
+            tx_ops.send(Op::UnpauseData(th)).unwrap();
+        }
+
+        match rx_data.try_recv() {
+            Ok(Some(data)) => {
+                eprint!("{}", std::str::from_utf8(&data).unwrap());
+                thread::sleep(Duration::from_millis(100));
+            }
+            Ok(None) => {
+                if signal.is_send_failed() {
+                    tx_ops.send(Op::UnpauseData(th)).unwrap();
+                }
+                atomic_wait::wait(signal.signal(), SIG_EVENT);
+            }
+            Err(ReceiveError::SendClosed) => break,
+            Err(ReceiveError::Closed) => unreachable!(),
+        }
     }
     handle.join().unwrap();
 }
--- crates/fast-down/src/curl/worker.rs
+++ crates/fast-down/src/curl/worker.rs
@@ -1,9 +1,12 @@
 use bytes::Bytes;
-use curl::easy::{Easy2, HttpVersion, List};
 use curl::easy::{Easy, Handler, WriteError};
+use curl::easy::{Easy2, HttpVersion, List};
 use curl::multi::Multi;
-use std::collections::VecDeque;
 use kanal::{ReceiveError, Receiver, SendError, Sender};
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::sync::atomic::{AtomicBool, AtomicU32, AtomicU8, Ordering};
+use std::sync::{Arc, Mutex};
 
 #[derive(Default, Clone)]
 struct BufferedDataCollector {
@@ -25,7 +28,7 @@ impl Handler for BufferedDataCollector {
         Ok(data.len())
     }
 }
-struct ChannelDataCollector(Sender<Bytes>);
+pub struct ChannelDataCollector(Arc<DataSignal>, Sender<Bytes>);
 
 impl Handler for ChannelDataCollector {
     fn header(&mut self, header: &[u8]) -> bool {
@@ -35,10 +38,16 @@ impl Handler for ChannelDataCollector {
 
     fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
         let bytes = Bytes::copy_from_slice(data);
-        match self.0.try_send(bytes) {
-            Ok(true) => Ok(data.len()),
-            Ok(false) =>  Err(WriteError::Pause),
-            e@ Err(SendError::ReceiveClosed | SendError::Closed) => {
+        match self.1.try_send(bytes) {
+            Ok(true) => {
+                self.0.notify(false);
+                Ok(data.len())
+            }
+            Ok(false) => {
+                self.0.notify(true);
+                Err(WriteError::Pause)
+            }
+            e @ Err(SendError::ReceiveClosed | SendError::Closed) => {
                 e.unwrap();
                 unreachable!()
             }
@@ -46,11 +55,143 @@ impl Handler for ChannelDataCollector {
     }
 }
 
-#[derive(Clone, Debug)]
+enum SignalWaker {
+    Waker(Arc<Mutex<Option<core::task::Waker>>>),
+    Callback(Arc<Mutex<Box<dyn FnMut(u32) + Send + Sync>>>),
+    None,
+}
+
+
+pub struct DataSignal {
+    waker: SignalWaker,
+    state: AtomicU32,
+    signal: AtomicU32,
+}
+
+pub const STATE_SEND_FAILED: u32 = 2;
+pub const STATE_SENT: u32 = 1;
+pub const STATE_NONE: u32 = 0;
+
+pub const SIG_NONE: u32 = 2;
+pub const SIG_EVENT: u32 = 1;
+
+impl Default for DataSignal {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl DataSignal {
+    pub fn new_with_waker(maybe_waker_mutex: Arc<Mutex<Option<core::task::Waker>>>) -> Self {
+        Self {
+            waker: SignalWaker::Waker(maybe_waker_mutex),
+            state: AtomicU32::new(STATE_NONE),
+            signal: AtomicU32::new(SIG_NONE),
+        }
+    }
+    pub fn new_with_callback(
+        maybe_fn_mutex: Arc<Mutex<Box<dyn FnMut(u32) + Send + Sync>>>,
+    ) -> Self {
+        Self {
+            waker: SignalWaker::Callback(maybe_fn_mutex),
+            state: AtomicU32::new(STATE_NONE),
+            signal: AtomicU32::new(SIG_NONE),
+        }
+    }
+    pub fn new() -> Self {
+        Self {
+            waker: SignalWaker::None,
+            state: AtomicU32::new(STATE_NONE),
+            signal: AtomicU32::new(SIG_NONE),
+        }
+    }
+
+    pub fn state(&self) -> u32 {
+        self.state.load(Ordering::Acquire)
+    }
+
+    pub fn signal(&self) -> &AtomicU32 {
+        &self.signal
+    }
+
+    fn reset_from(&self, old_state: Option<u32>, old_signal: Option<u32>) -> bool {
+        if let Some(state) = old_state {
+            match self.state.compare_exchange(state, STATE_NONE, Ordering::Acquire, Ordering::Acquire) {
+                Ok(_) => {},
+                Err(_) => return false
+            }
+        } else {
+            self.state.store(STATE_NONE, Ordering::Release);
+        }
+        if let Some(signal) = old_signal {
+            match self.signal.compare_exchange(signal, SIG_NONE, Ordering::Acquire, Ordering::Acquire) {
+                Ok(_) => {},
+                Err(_) => return false
+            }
+        } else {
+            self.signal.store(SIG_NONE, Ordering::Release);
+        }
+        true
+    }
+
+    pub fn is_send_failed(&self) -> bool {
+        match self.state.load(Ordering::Acquire) {
+            STATE_SEND_FAILED => true,
+            STATE_NONE | STATE_SENT => false,
+            _ => unreachable!("invalid state"),
+        }
+    }
+
+    pub fn is_sent(&self) -> bool {
+        match self.state.load(Ordering::Acquire) {
+            STATE_SENT => true,
+            STATE_NONE | STATE_SEND_FAILED => false,
+            _ => unreachable!("invalid state"),
+        }
+    }
+
+    pub fn is_none(&self) -> bool {
+        match self.state.load(Ordering::Acquire) {
+            STATE_NONE => true,
+            STATE_SENT | STATE_SEND_FAILED => false,
+            _ => unreachable!("invalid state"),
+        }
+    }
+
+    fn notify(&self, send_fail: bool) {
+        let val = if send_fail { 2 } else { 1 };
+        self.state.store(val, Ordering::Release);
+        self.signal.store(1, Ordering::Release);
+        atomic_wait::wake_all(&self.signal);
+        self.signal.store(STATE_NONE, Ordering::Release);
+        match &self.waker {
+            SignalWaker::Waker(waker) => {
+                if let Some(a) = waker.lock().unwrap().take() {
+                    core::task::Waker::wake(a)
+                }
+            }
+            SignalWaker::Callback(f) => f.lock().unwrap()(val),
+            SignalWaker::None => {}
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
 pub struct TaskHandle(usize);
+pub mod options {
+    use super::*;
+    pub struct New {
+        pub headers: List,
+        pub url: String,
+        pub signal: Arc<DataSignal>,
+        pub extra: Option<
+            Box<dyn FnOnce(Easy2<ChannelDataCollector>) -> Easy2<ChannelDataCollector> + Send>,
+        >,
+    }
+}
 
 pub enum Op {
-    New(Sender<Bytes>, List, String, oneshot::Sender<TaskHandle>),
+    New(Sender<Bytes>, options::New, oneshot::Sender<TaskHandle>),
     Kill(TaskHandle),
     UnpauseData(TaskHandle),
 }
@@ -62,36 +203,47 @@ pub fn multi(rx_ops: Receiver<Op>) -> Result<(), anyhow::Error> {
         let maybe_task = match rx_ops.try_recv() {
             Ok(maybe_task) => maybe_task,
             Err(ReceiveError::Closed) => break Ok(()),
-            Err(ReceiveError::SendClosed) => unreachable!()
+            Err(ReceiveError::SendClosed) => unreachable!(),
         };
         match maybe_task {
             None => {}
-            Some(Op::New(tx, headers, url, ret)) => {
-                let mut easy2 = Easy2::new(ChannelDataCollector(tx));
+            Some(Op::New(
+                tx,
+                options::New {
+                    headers,
+                    url,
+                    signal,
+                    extra,
+                },
+                ret,
+            )) => {
+                let mut easy2 = Easy2::new(ChannelDataCollector(signal, tx));
                 easy2.url(&url)?;
-                easy2.ssl_verify_host(false)?; // todo(CyanChanges): temporary workaround, might be changed later
-                easy2.ssl_verify_peer(false)?; // todo(CyanChanges): temporary workaround, might be changed later
                 easy2.http_headers(headers)?;
-                easy2.http_version(HttpVersion::V3)?;
-                ret.send(TaskHandle(handles.insert(multi.add2(easy2)?))).expect("cannot send back handle");
+                let easy2 = if let Some(f) = extra { f(easy2) } else { easy2 };
+                ret.send(TaskHandle(handles.insert(multi.add2(easy2)?)))
+                    .expect("cannot send back handle");
             }
             Some(Op::Kill(TaskHandle(key))) => {
-                let handle = handles.try_remove(key)
+                let handle = handles
+                    .try_remove(key)
                     .ok_or_else(|| format!("no such handle: {key}"))
                     .unwrap();
                 multi.remove2(handle)?;
-            },
+            }
             Some(Op::UnpauseData(TaskHandle(key))) => {
-                let handle = handles.get(key)
+                let handle = handles
+                    .get(key)
                     .ok_or_else(|| format!("no such handle: {key}"))
                     .unwrap();
+                handle.get_ref().0.reset_from(None, None);
                 handle.unpause_write()?;
             }
         }
         match multi.perform() {
             Ok(0) => break Ok(()),
             Ok(_) => continue,
-            Err(e) => return Err(anyhow::anyhow!(e))
+            Err(e) => return Err(anyhow::anyhow!(e)),
         }
     }
 }
