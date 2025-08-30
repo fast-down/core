5bb026270012a79e9e50544e2d10d331b518602a
share121 <me@s121.top>
2025-08-30 08:47:55 +0800

    fix: 修复遗漏导出 api 的问题

--- crates/fast-down/src/lib.rs
+++ crates/fast-down/src/lib.rs
@@ -1,10 +1,9 @@
 mod url_info;
 
 pub use fast_pull::*;
-pub use url_info::UrlInfo;
+pub use url_info::*;
 
+#[cfg(feature = "http")]
 pub mod http;
 #[cfg(feature = "reqwest")]
 pub mod reqwest;
-// #[cfg(feature = "reqwest-middleware")]
-// pub mod reqwest_middleware;
